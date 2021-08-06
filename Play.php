07224c4ab2cfdc7d371ab384a017acb63347db91
azkalonz <aeartixenter@gmail.com>
2021-08-06 15:02:40 +0800

    fix

--- Play.php
+++ Play.php
@@ -1,1 +1,30 @@
-<embed id="gameSWF" src="gamefiles/AW_loader.swf" width="960" height="550" loop="false" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" scale="exactfit" allowscriptaccess="always" allowFullScreen="true" menu="false" flashvars="&sLang=en"></embed>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Play</title>
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      background: #000;
+      display: flex;
+      height: 100vh;
+      width: 100vw;
+      justify-content: center;
+    }
+
+    embed {
+      margin-top: 40px;
+    }
+  </style>
+</head>
+
+<body>
+  <embed id="gameSWF" src="gamefiles/AW_loader.swf" width="960" height="550" loop="false" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" scale="exactfit" allowscriptaccess="always" allowFullScreen="true" menu="false" flashvars="&sLang=en"></embed>
+</body>
+
+</html>
